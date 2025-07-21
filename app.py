8006c2e47ae1636b065a3950dc3bdba747dfe32e
DanielQu9_mac <zxc86796139@gmail.com>
2025-07-22 01:55:51 +0800

    修正: 新增get_esp_data處理資料

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from depot import Depot
+from depot import Depot, DepotItem, DepotError
+import threading
 
 
 # ==== 初始化設定 ====
@@ -7,30 +8,46 @@ app = Flask(__name__)
 depot = Depot()
 
 
-@app.route('/')
+@app.route("/")
 def index():
     inventory = depot.get_inventory()
     items = list(inventory.keys()) if inventory else None
-    return render_template('index.html', items=items)
+    return render_template("index.html", items=items)
 
-@app.route('/api/data')
+
+@app.route("/api/data")
 def api_data():
     """回傳: item, count, unit_weight, min_weight_warning"""
-    item = request.args.get('item')
+    item = request.args.get("item")
     if not item:
         return jsonify({})
     inventory = depot.get_inventory()
     # 明確指定要查 tag 的 item
     tag = depot.get_tag_json(item)
     count = inventory.get(item, 0) if inventory else 0
-    unit_w = tag.get('unit_weight', 0) if tag else None
-    min_w = tag.get('min_weight_warning', 0) if tag else None
-    return jsonify({
-        'item': item,
-        'count': count,
-        'unit_weight': unit_w,
-        'min_weight': min_w
-    })
+    unit_w = tag.get("unit_weight", 0) if tag else None
+    min_w = tag.get("min_weight_warning", 0) if tag else None
+    return jsonify(
+        {"item": item, "count": count, "unit_weight": unit_w, "min_weight": min_w}
+    )
 
-if __name__ == '__main__':
+
+@app.route("/api/esp_send", methods=["POST"])
+def get_esp_data():
+    data = request.get_json()
+    if (not data) or ("item" not in data) or ("amount" not in data):
+        return jsonify({"status": "error", "message": "deta_error"}), 400
+
+    # 處理資料
+    try:
+        depot.write(DepotItem(type="auto", item=data["item"], amount=data["amount"]))
+        print(f"[Depot]-info: write_down")
+    except DepotError as err:
+        print(f"[Depot]-error: {err}")
+        return jsonify({"status": "error", "message": f"deta_bad: {err}"})
+
+    return jsonify({"status": "success", "message": "deta_ok"})
+
+
+if __name__ == "__main__":
     app.run(debug=True)
