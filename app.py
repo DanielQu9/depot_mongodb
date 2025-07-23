eaf6e2c9d5790404ebc5561a8eb89b8befb1bb35
DanielQu9_mac <zxc86796139@gmail.com>
2025-07-24 00:18:44 +0800

    新增: 整合 DepotMongo 類別，更新庫存和紀錄路由以支持 MongoDB 操作

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from depot import Depot, DepotItem, DepotError
+from depot import Depot, DepotItem, DepotError, DepotMongo
 
 
 # ==== 初始化設定 ====
 app = Flask(__name__)
 depot = Depot()
+mg = DepotMongo()
 
 
 @app.route("/")
@@ -13,6 +14,9 @@ def index():
     side_items = [
         {"name": "首頁", "endpoint": "home"},
         {"name": "倉庫", "endpoint": "inventory"},
+        {"name": "出/入貨物", "endpoint": "inventory"},
+        {"name": "貨物紀錄", "endpoint": "records"},
+        {"name": "狀態", "endpoint": "inventory"},
         # 日後再加頁面，就在這邊添加
     ]
     return render_template("base.html", side_items=side_items)
@@ -20,12 +24,30 @@ def index():
 
 @app.route("/home")
 def home():
+    """首頁"""
     return render_template("home.html")
 
 
 @app.route("/inventory")
 def inventory():
-    return render_template("inventory.html")
+    """倉庫庫存"""
+    inv = depot.get_inventory()
+    return render_template("inventory.html", items=inv)
+
+
+@app.route("/records")
+def records():
+    """進出貨紀錄-輸出列表"""
+    table_list = mg.date_collections  # 獲取所有資料表
+    return render_template("records.html", tables=table_list)
+
+
+@app.route("/records/data")
+def records_data():
+    """進出貨紀錄-輸出紀錄"""
+    table_name = request.args.get("date")
+    data = mg.find_records(str(table_name))
+    return render_template("records_data.html", data=data, date=table_name)
 
 
 @app.route("/api/data")
@@ -58,9 +80,9 @@ def get_esp_data():
         big = DepotItem("out", "big", data["big"])
         tube = DepotItem("out", "tube", data["tube"])
 
-        depot.write(small, "web")
-        depot.write(big, "web")
-        depot.write(tube, "web")
+        depot.write(small, "esp")
+        depot.write(big, "esp")
+        depot.write(tube, "esp")
         print(f"[Depot]-info: write_down")
     except DepotError as err:
         print(f"[Depot]-error: {err}")
