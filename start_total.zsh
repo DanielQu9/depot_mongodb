830ce47689ce5a41e525e2ec22b09724acd88e0a
DanielQu9_mac <zxc86796139@gmail.com>
2025-08-18 15:28:02 +0800

    修正: 更新start_total.zsh中的虛擬環境路徑

--- start_total.zsh
+++ start_total.zsh
@@ -9,12 +9,12 @@ echo "Current directory: $(pwd)"
 CURRENT_DIR=$(pwd)
 
 # 在新Terminal視窗中執行第一個Python檔案
-osascript -e "tell application \"Terminal\" to do script \"cd '$CURRENT_DIR' && source .venv/bin/activate && python app.py\""
+osascript -e "tell application \"Terminal\" to do script \"cd '$CURRENT_DIR' && source .././.venv/bin/activate && python app.py\""
 
 # 在新Terminal視窗中執行第二個Python檔案
-osascript -e "tell application \"Terminal\" to do script \"cd '$CURRENT_DIR' && source .venv/bin/activate && python line.py\""
+osascript -e "tell application \"Terminal\" to do script \"cd '$CURRENT_DIR' && source .././.venv/bin/activate && python line.py\""
 
 # 在新Terminal視窗中執行第三個Python檔案
-osascript -e "tell application \"Terminal\" to do script \"cd '$CURRENT_DIR' && source .venv/bin/activate && python start_dns.py\""
+osascript -e "tell application \"Terminal\" to do script \"cd '$CURRENT_DIR' && source .././.venv/bin/activate && python start_dns.py\""
 
 echo "All Python applications started."
