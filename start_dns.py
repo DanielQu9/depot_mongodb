53e0cc5d512e50b3946560798b3282c5869810f6
dxq <zxc86796139@gmail.com>
2025-09-25 15:54:07 +0800

    更新: 調整API填寫提示，將錯誤處理移至try-except區塊以增強程式穩定性

--- start_dns.py
+++ start_dns.py
@@ -9,12 +9,16 @@ if __name__ == "__main__":
         token_str = 'LINE_CHANNEL_SECRET=""\nLINE_CHANNEL_ACCESS_TOKEN=""\nLINE_DNS_TOKEN=""\nWEB_DNS_TOKEN=""'
         with open(file="./.env", mode="w", encoding="utf-8") as file:
             file.write(token_str)
-            print("請填寫api")
+            print(">> 請填寫api")
         os._exit(0)
 
     env = dotenv_values()
-    line = f'cloudflared tunnel run --token {env["LINE_DNS_TOKEN"]}'
-    web = f'cloudflared tunnel run --token {env["WEB_DNS_TOKEN"]}'
+    try:
+        line = f'cloudflared tunnel run --token {env["LINE_DNS_TOKEN"]}'
+        web = f'cloudflared tunnel run --token {env["WEB_DNS_TOKEN"]}'
+    except KeyError:
+        print(">> 請填寫api")
+        os._exit(0)
 
     if os.name == "nt":
         os.system(f"start cmd /k {line}")
