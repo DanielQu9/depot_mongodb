1151fe97b7003a65949ea7276fc7e3886b3073c5
dxq <zxc86796139@gmail.com>
2025-09-25 15:55:05 +0800

    更新: 移除中文註解防止亂碼

--- start_total.bat
+++ start_total.bat
@@ -2,17 +2,17 @@
 echo Starting Python applications...
 
 chcp 65001
-:: 設定工作目錄為批次檔所在目錄
+
 cd /d "%~dp0"
 echo Current directory: %cd%
 
-:: 在新視窗中執行第一個Python檔案
+
 start "Python App 1" cmd /k "cd /d "%~dp0" && python app.py"
 
-:: 在新視窗中執行第二個Python檔案
+
 start "Python App 2" cmd /k "cd /d "%~dp0" && python line.py"
 
-:: 在新視窗中執行第三個Python檔案
+
 start "Python App 3" cmd /k "cd /d "%~dp0" && python start_dns.py"
 
 echo All Python applications started.
